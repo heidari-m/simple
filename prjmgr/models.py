54308aa5e2b5ce7a84f55e12422500d9e5d863cb
mehdi <heidari_mahdi84@yahoo.com>
2019-10-08 19:00:03 +0330

    change BillOfLading model

--- prjmgr/models.py
+++ prjmgr/models.py
@@ -107,6 +107,7 @@ class Shipping(models.Model):
 class BillOfLading(models.Model):
     delivery_date = models.DateField(null=True, blank=True)
     BL_number = models.CharField(max_length=25, null=True, blank=True)
+    contract = models.ForeignKey('Contract', on_delete=models.SET_NULL, null=True, blank=True )
     shipping = models.ForeignKey(Shipping, on_delete=models.SET_NULL, null=True, blank=True)
     receipnt = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     amount = models.DecimalField(max_digits=20, decimal_places=4)
--- prjmgr/views.py
+++ prjmgr/views.py
@@ -48,6 +48,7 @@ class ContractDetailView(LoginRequiredMixin,SingleTableMixin, generic.DetailView
         # context['table2'] = ContractOperationTable(qs2)
         qs3 = Operation.objects.filter(contract=self.kwargs['C_No']).filter(operation_type='tank_out').values('customs_clearance_number').annotate(Sum('amount_mt'))
         context['table3'] = TmpTable(qs3)
+        qs4 = BillOfLading.objects.filter(contract=self.kwargs['C_No'])
         return context
 
 
